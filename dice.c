a5ff7d527a50047c33dda09184405bc1bd19b541
Jumpei Kanno <s1260028@std1dc28.u-aizu.ac.jp>
2019-07-05 12:09:29 +0900

    Won/lost message added

--- dice.c
+++ dice.c
@@ -10,6 +10,8 @@ int main(){
   b = rand()%6+1;
 
   printf("Die 1:%d\nDie 2:%d\n",a,b);
-  printf("Total value: %d\n",a+b); 
+  printf("Total value: %d\n",a+b);
+  if(a+b>7) printf("You won\n");
+  else printf("You lost\n");
   return 0;
 }
