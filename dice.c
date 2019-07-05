117f4c0d1bd568eca67ac485565551f0aaed39d5
Jumpei Kanno <s1260028@std1dc28.u-aizu.ac.jp>
2019-07-05 11:58:02 +0900

    Greetings added

--- dice.c
+++ dice.c
@@ -3,12 +3,18 @@
 #include<time.h>
 
 int main(){
+   char name[50];
   int a,b;
+  printf("What is your name?\n");
+  printf("> ");
+  scanf("%s",name);
+  printf("Hello,%s!\n",name);
   printf("Rolling the dice...\n");
   srand(time(NULL));
   a = rand()%6+1;
   b = rand()%6+1;
 
   printf("Die 1:%d\nDie 2:%d\n",a,b);
+  printf("Total value: %d\n",a+b);
   return 0;
 }
