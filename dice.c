7aeb5830baaf8769d1c91f124b5fac0137c16c30
s1260028 <s1260028@call1dc23.u-aizu.ac.jp>
2019-07-05 15:02:40 +0900

    Add second dice roll

--- dice.c
+++ dice.c
@@ -18,5 +18,16 @@ int main(){
   printf("Total value: %d\n",a+b);
   if(a+b>7) printf("%s won!\n",name);
   else printf("%s lost!\n",name);
+  
+  printf("Second rolling the dice...\n");
+  srand(time(NULL));
+  a = rand()%6+1;
+  b = rand()%6+1;
+
+  printf("Die 1:%d\nDie 2:%d\n",a,b);
+  printf("Total value: %d\n",a+b);
+  if(a+b>7) printf("%s won!\n",name);
+  else printf("%s lost!\n",name);
+  
   return 0;
 }
